c7fccc539d78ad3268614cb9a8b1021965ecd28e
Eina Onting <einers@outlook.com>
2018-11-03 20:17:51 -0700

    clean up svg files further + display them on page when selected

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import types from './carrot-types';
+
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      selectedType: null
+    }
+  }
+
+  selectAType(carrotType) {
+    this.setState({ selectedType: carrotType })
+  }
+
   render() {
+    console.log('what is the selected carrot', this.state)
     return (
       <div className="App">
-      <p>Hey sup</p>
+        <h1>Build a Carrot</h1>
+
+        <div>
+          {Object.keys(types).map((carrot, idx) => (
+            <button
+              key={`${types[carrot].name}-${idx}`}
+              onClick={() => this.selectAType(carrot)}
+            >
+              {types[carrot].name}</button>
+          )
+          )}
+        </div>
+
+        {this.state.selectedType && (
+          <div className="svg-container">
+            {types[this.state.selectedType].svg}
+          </div>
+        )}
       </div>
     );
   }
---
+++ src/carrot-types.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Chantenay from './jsx-svg/chantenay';
+import Danvers from './jsx-svg/danvers';
+import Imperator from './jsx-svg/imperator';
+import Nantes from './jsx-svg/nantes';
+import ParisMarket from './jsx-svg/paris-market';
+
+export default {
+  "chantenay": {
+    name: "chantenay",
+    svg: <Chantenay />
+  },
+  "danvers": {
+    name: "danvers",
+    svg: <Danvers />
+  },
+  "imperator": {
+    name: "imperator",
+    svg: <Imperator />
+  },
+  "nantes": {
+    name: "nantes",
+    svg: <Nantes />
+  },
+  "paris-market": {
+    name: "paris market",
+    svg: <ParisMarket />
+  }
+}
